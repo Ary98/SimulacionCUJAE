44c64f7ae7f4aa50114daab64ad97f1be70d79ca
Ary98 <47032008+Ary98@users.noreply.github.com>
2020-01-15 09:31:13 -0800

    Subidos resto de ficheros

---
+++ generate spatialite.bat
@@ -0,0 +1,2 @@
+spatialite_osm_net -o ciudad_deportiva.osm -d ciudad_deportiva.osm.db -T roads -tf road_template.cfg
+# spatialite_osm_net -o ciudad_deportiva.osm -d ciudad_deportiva.osm.db -T rails
